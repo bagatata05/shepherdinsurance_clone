6303ec853770cfd3cc60b45c8b7e682da1cd3e9a
Naphier Awalie <naphiera@gmail.com>
2026-06-20 00:01:12 +0800

    Create DurabilitySection.tsx

---
+++ src/app/products/builders-risk/components/DurabilitySection.tsx
@@ -0,0 +1,244 @@
+import React from "react";
+
+export const DurabilitySection: React.FC = () => {
+  return (
+    <section className="u-section u-theme-dark">
+      <div id="careers-video" className="u-visual">
+        <img
+          src="/assets/images/rectangle-171.webp"
+          loading="lazy"
+          alt=""
+          sizes="(max-width: 1360px) 100vw, 1360px"
+          srcSet="https://cdn.prod.website-files.com/6941fd8f651ca29cd5bc3597/696113dff04f3eafcb2e413b_Rectangle%20171-p-500.webp 500w, https://cdn.prod.website-files.com/6941fd8f651ca29cd5bc3597/696113dff04f3eafcb2e413b_Rectangle%20171-p-800.webp 800w, https://cdn.prod.website-files.com/6941fd8f651ca29cd5bc3597/696113dff04f3eafcb2e413b_Rectangle%20171-p-1080.webp 1080w, https://cdn.prod.website-files.com/6941fd8f651ca29cd5bc3597/696113dff04f3eafcb2e413b_Rectangle%20171-p-1600.webp 1600w, https://cdn.prod.website-files.com/6941fd8f651ca29cd5bc3597/696113dff04f3eafcb2e413b_Rectangle%20171-p-2000.webp 2000w, https://cdn.prod.website-files.com/6941fd8f651ca29cd5bc3597/696113dff04f3eafcb2e413b_Rectangle%20171-p-2600.webp 2600w, /assets/images/rectangle-171.webp 2880w"
+          className="u-visual-image"
+        />
+        <div className="u-visual-bk"></div>
+        <div
+          style={{ opacity: "0.4" }}
+          className="u-visual-overlay"
+        ></div>
+        <div className="img-overlay"></div>
+      </div>
+      <div className="section_spacer">
+        <div className="u-container">
+          <div className="full-img-section">
+            <div className="u-content v-flex-img-section">
+              <div className="section-header-grid u-grid-custom">
+                <div className="section-header-title-col u-column-span-2">
+                  <div className="heading-icon">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      viewBox="0 0 17 34"
+                      fill="none"
+                    >
+                      <path
+                        d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                        fill="#DA43E7"
+                      ></path>
+                    </svg>
+                  </div>
+                  <h1
+                    data-max="ch25"
+                    className="section-heading u-text-style-h3"
+                  >
+                    Increase certainty and unlock savings.
+                  </h1>
+                </div>
+                <div>
+                  <div className="u-text-style-main">
+                    Technology and underwriting move together to remove
+                    friction, unlock speed, and increase certainty
+                  </div>
+                </div>
+              </div>
+              <div className="card-grid-3 u-grid-custom">
+                <div
+                  data-wf--centered-card--variant="transparent"
+                  className="centered-card-slide"
+                >
+                  <div className="centered-card-wrap w-variant-bb16849b-d250-e704-3f41-81eb2aacea29">
+                    <div className="centered-card-content-abs">
+                      <div className="div-block">
+                        <div className="div-block-2">
+                          <img
+                            src="/assets/gifs/speed-keeps-pace-white.gif"
+                            loading="lazy"
+                            alt=""
+                            className="img-abs"
+                          />
+                        </div>
+                      </div>
+                      <div className="u-text-style-large">
+                        Speed that keeps pace
+                      </div>
+                      <div>
+                        Underwriting decisions in hours,not weeks.
+                        Automation and AI streamline complexity.
+                      </div>
+                    </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      viewBox="0 0 60 60"
+                      fill="none"
+                      className="corner-svg w-variant-bb16849b-d250-e704-3f41-81eb2aacea29"
+                    >
+                      <path
+                        d="M60 60L60 -2.14577e-06L30 30L-2.14577e-06 60L60 60Z"
+                        fill="white"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="div-block-21">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="0px"
+                      height="0px"
+                      viewBox="0 0 440 440"
+                      fill="none"
+                      className="clip-svg"
+                    >
+                      <defs>
+                        <clipPath
+                          id="Subtract"
+                          clipPathUnits="objectBoundingBox"
+                        >
+                          <path
+                            d="M0.8636363636363636,-3.775068181818181e-8l0.13636363636363635,0.13636363636363635l0,0.8636363636363636l-1,0l0,-1l0.8636363636363636,0z"
+                            fill="black"
+                          ></path>
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </div>
+                </div>
+                <div
+                  data-wf--centered-card--variant="transparent"
+                  className="centered-card-slide"
+                >
+                  <div className="centered-card-wrap w-variant-bb16849b-d250-e704-3f41-81eb2aacea29">
+                    <div className="centered-card-content-abs">
+                      <div className="div-block">
+                        <div className="div-block-2">
+                          <img
+                            src="/assets/gifs/intelligence-rewards-white.gif"
+                            loading="lazy"
+                            alt=""
+                            className="img-abs"
+                          />
+                        </div>
+                      </div>
+                      <div className="u-text-style-large">
+                        Intelligence that rewards
+                      </div>
+                      <div>
+                        Data and technology to recognizesafer turning
+                        innovation into tangible financial advantage.
+                      </div>
+                    </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      viewBox="0 0 60 60"
+                      fill="none"
+                      className="corner-svg w-variant-bb16849b-d250-e704-3f41-81eb2aacea29"
+                    >
+                      <path
+                        d="M60 60L60 -2.14577e-06L30 30L-2.14577e-06 60L60 60Z"
+                        fill="white"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="div-block-21">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="0px"
+                      height="0px"
+                      viewBox="0 0 440 440"
+                      fill="none"
+                      className="clip-svg"
+                    >
+                      <defs>
+                        <clipPath
+                          id="Subtract"
+                          clipPathUnits="objectBoundingBox"
+                        >
+                          <path
+                            d="M0.8636363636363636,-3.775068181818181e-8l0.13636363636363635,0.13636363636363635l0,0.8636363636363636l-1,0l0,-1l0.8636363636363636,0z"
+                            fill="black"
+                          ></path>
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </div>
+                </div>
+                <div
+                  data-wf--centered-card--variant="transparent"
+                  className="centered-card-slide"
+                >
+                  <div className="centered-card-wrap w-variant-bb16849b-d250-e704-3f41-81eb2aacea29">
+                    <div className="centered-card-content-abs">
+                      <div className="div-block">
+                        <div className="div-block-2">
+                          <img
+                            src="/assets/gifs/single-connected-system-white.gif"
+                            loading="lazy"
+                            alt=""
+                            className="img-abs"
+                          />
+                        </div>
+                      </div>
+                      <div className="u-text-style-large">
+                        A single connected system
+                      </div>
+                      <div>
+                        Our products work as one unified ecosystem,
+                        designed around how real projects evolve not
+                        traditional siloes.
+                      </div>
+                    </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      viewBox="0 0 60 60"
+                      fill="none"
+                      className="corner-svg w-variant-bb16849b-d250-e704-3f41-81eb2aacea29"
+                    >
+                      <path
+                        d="M60 60L60 -2.14577e-06L30 30L-2.14577e-06 60L60 60Z"
+                        fill="white"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="div-block-21">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="0px"
+                      height="0px"
+                      viewBox="0 0 440 440"
+                      fill="none"
+                      className="clip-svg"
+                    >
+                      <defs>
+                        <clipPath
+                          id="Subtract"
+                          clipPathUnits="objectBoundingBox"
+                        >
+                          <path
+                            d="M0.8636363636363636,-3.775068181818181e-8l0.13636363636363635,0.13636363636363635l0,0.8636363636363636l-1,0l0,-1l0.8636363636363636,0z"
+                            fill="black"
+                          ></path>
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
