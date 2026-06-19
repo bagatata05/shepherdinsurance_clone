c408283fac85d50992403c727efea18131d5b862
Naphier Awalie <naphiera@gmail.com>
2026-06-20 00:01:03 +0800

    Create SupportSection.tsx

---
+++ src/app/products/builders-risk/components/SupportSection.tsx
@@ -0,0 +1,305 @@
+import React from "react";
+
+export const SupportSection: React.FC = () => {
+  return (
+    <section className="u-section u-theme-grey">
+      <div className="section_spacer">
+        <div className="u-container">
+          <div className="u-content v-flex-8">
+            <div className="content-flex v-flex-8">
+              <div className="section-header-grid u-grid-custom">
+                <div className="section-header-title-col u-column-span-2">
+                  <div className="heading-icon">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="100%"
+                      viewBox="0 0 17 34"
+                      fill="none"
+                    >
+                      <path
+                        d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                        fill="#DA43E7"
+                      ></path>
+                    </svg>
+                  </div>
+                  <h1
+                    data-max="ch25"
+                    className="section-heading u-text-style-h3"
+                  >
+                    Comprehensive support from submission to renewal.
+                  </h1>
+                </div>
+              </div>
+              <div className="submission-grid u-grid-custom">
+                <div className="submission-flex">
+                  <div className="u-text-style-main">
+                    Product Offering
+                  </div>
+                  <div className="support-bottom">
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Project-Specific Placements
+                      </div>
+                    </div>
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Master Programs
+                      </div>
+                    </div>
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Quota Share
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="submission-flex">
+                  <div className="u-text-style-main">
+                    Coverage Capabilities
+                  </div>
+                  <div className="support-bottom">
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Catastrophe (CAT) Coverage
+                      </div>
+                    </div>
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Testing & Mechanical Break down
+                      </div>
+                    </div>
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Modified Defective Design
+                      </div>
+                    </div>
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Green Plus Coverage
+                      </div>
+                    </div>
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Contigent Builders RIsk
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="submission-flex">
+                  <div className="u-text-style-main">
+                    Structure & Capacity
+                  </div>
+                  <div className="support-bottom">
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Paper: A.M. Best A+ (Superior), FSC XV
+                      </div>
+                    </div>
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Target project TIV up to $75M
+                      </div>
+                    </div>
+                    <div className="products-flex">
+                      <div className="checkmark">
+                        <div className="solution-check">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 18 18"
+                            fill="none"
+                            className="check-svg"
+                          >
+                            <path
+                              d="M3.5 10L7 13.3094L14.5 4"
+                              stroke="white"
+                              strokeWidth="2"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="check-txt u-text-style-large">
+                        Quota share participation on larger projects {">"}{" "}
+                        $150M TIV
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
