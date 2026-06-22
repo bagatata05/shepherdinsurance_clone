c7e0009c183fbc91af40a5e7cdbadda967a8ecc4
Naphier Awalie <naphiera@gmail.com>
2026-06-22 20:14:38 +0800

    Create ScrollDropdowns.tsx

---
+++ src/app/industry/renewable-energy-power/components/ScrollDropdowns.tsx
@@ -0,0 +1,355 @@
+"use client";
+
+import React from "react";
+
+export const ScrollDropdowns: React.FC = () => {
+  return (
+    <section className="u-section u-theme-beige">
+      <div className="section_spacer no-bottom-t">
+        <div className="forced-scroll-section-height">
+          <div className="forced-scroll-section">
+            <div className="u-container">
+              <div className="u-content v-flex-8">
+                <div className="content-flex v-flex-6">
+                  <div className="section-content-flex v-flex-5">
+                    <div className="section-header-grid u-grid-custom">
+                      <div className="section-header-title-col u-column-span-2">
+                        <div className="heading-icon">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            viewBox="0 0 17 34"
+                            fill="none"
+                          >
+                            <path
+                              d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                              fill="#DA43E7"
+                            ></path>
+                          </svg>
+                        </div>
+                        <h1
+                          data-max="ch25"
+                          className="section-heading u-text-style-h3"
+                        >
+                          Powered by tech. Proven in the field.
+                        </h1>
+                      </div>
+                      <div>
+                        <div className="u-text-style-main">
+                          Learn how Shepherd integrates smarter
+                          technology, and operators are raising the bar
+                          for their industries.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="scroll-dropdown-flex">
+                    <div className="scroll-dropdown-item">
+                      <div className="scroll-dropdown-top">
+                        <div className="scroll-dropdown-top-text u-text-style-h4">
+                          Coordinated Placement for Utility-Scale Solar
+                          Contractor
+                        </div>
+                      </div>
+                      <div className="scroll-dropdown-bottom-wrap">
+                        <div className="scroll-dropdown-bottom-grid u-grid-custom">
+                          <div className="scroll-dropdown-bottom-video-col u-column-span-2">
+                            <div>
+                              <div
+                                video-parent=""
+                                className="scroll-dropdown-bottom-video-wrap"
+                              >
+                                <div
+                                  id="careers-video"
+                                  className="u-visual"
+                                >
+                                  <img
+                                    src="/assets/images/shawn-rain-he-rebzofui-unsplash-1.webp"
+                                    loading="lazy"
+                                    alt=""
+                                    sizes="(max-width: 1360px) 100vw, 1360px"
+                                    srcSet="/assets/images/shawn-rain-he-rebzofui-unsplash-1-p-500.webp 500w, /assets/images/shawn-rain-he-rebzofui-unsplash-1-p-800.webp 800w, /assets/images/shawn-rain-he-rebzofui-unsplash-1-p-1080.webp 1080w, /assets/images/shawn-rain-he-rebzofui-unsplash-1-p-1600.webp 1600w, /assets/images/shawn-rain-he-rebzofui-unsplash-1.webp 1920w"
+                                    className="u-visual-image"
+                                  />
+                                  <div className="u-visual-bk"></div>
+                                  <div className="img-overlay"></div>
+                                </div>
+                                <div className="abs-video-btn"></div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="scroll-dropdown-bottom-content">
+                            <div className="u-text-style-main">
+                              &quot;What further strengthened the
+                              opportunity was the organization&apos;s
+                              intentional use of drone technology for
+                              jobsite monitoring and oversight. This
+                              approach aligns closely with Shepherd&apos;s
+                              belief that technology can effectively
+                              improve visibility, safety, and risk
+                              management in construction operations.&quot;
+                            </div>
+                            <div>
+                              <div
+                                data-wf--button-main--variant="primary"
+                                className="button_main_wrap"
+                                data-button=""
+                                data-trigger="hover-focus"
+                              >
+                                <div className="clickable_wrap u-cover-absolute">
+                                  <a
+                                    href="/case-studies/coordinated-placement-for-utility-scale-solar-contractor"
+                                    className="clickable_link w-inline-block"
+                                  >
+                                    <span className="clickable_text u-sr-only">
+                                      READ MORE
+                                    </span>
+                                  </a>
+                                  <button
+                                    type="button"
+                                    className="clickable_btn"
+                                  >
+                                    <span className="clickable_text u-sr-only">
+                                      READ MORE
+                                    </span>
+                                  </button>
+                                </div>
+                                <div className="button_main_element">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="100%"
+                                    viewBox="0 0 12 16"
+                                    fill="none"
+                                    className="video-play"
+                                  >
+                                    <path
+                                      d="M11.25 7.79443L2.19575e-07 15.5887L9.00968e-07 0.000204548L11.25 7.79443Z"
+                                      fill="black"
+                                    ></path>
+                                  </svg>
+                                  <div
+                                    aria-hidden="true"
+                                    className="button_main_text u-text-style-small"
+                                  >
+                                    READ MORE
+                                  </div>
+                                  <div className="button_main_icon u-hide-if-empty"></div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="scroll-dropdown-item">
+                      <div className="scroll-dropdown-top">
+                        <div className="scroll-dropdown-top-text u-text-style-h4">
+                          Data Center Developer opts into Shepherd Savings
+                          for $315M OCIP in VA
+                        </div>
+                      </div>
+                      <div className="scroll-dropdown-bottom-wrap">
+                        <div className="scroll-dropdown-bottom-grid u-grid-custom">
+                          <div className="scroll-dropdown-bottom-video-col u-column-span-2">
+                            <div>
+                              <div
+                                video-parent=""
+                                className="scroll-dropdown-bottom-video-wrap"
+                              >
+                                <div
+                                  id="careers-video"
+                                  className="u-visual"
+                                >
+                                  <img
+                                    src="/assets/images/getty-images-0kdaczykk6y-unsplash-1_1x.webp"
+                                    loading="lazy"
+                                    alt=""
+                                    sizes="(max-width: 1360px) 100vw, 1360px"
+                                    srcSet="/assets/images/getty-images-0kdaczykk6y-unsplash-1_1x-p-500.webp 500w, /assets/images/getty-images-0kdaczykk6y-unsplash-1_1x-p-800.webp 800w, /assets/images/getty-images-0kdaczykk6y-unsplash-1_1x-p-1080.webp 1080w, /assets/images/getty-images-0kdaczykk6y-unsplash-1_1x-p-1600.webp 1600w, /assets/images/getty-images-0kdaczykk6y-unsplash-1_1x-p-2000.webp 2000w, /assets/images/getty-images-0kdaczykk6y-unsplash-1_1x-p-2600.webp 2600w, /assets/images/getty-images-0kdaczykk6y-unsplash-1_1x-p-3200.webp 3200w, /assets/images/getty-images-0kdaczykk6y-unsplash-1_1x.webp 4096w"
+                                    className="u-visual-image"
+                                  />
+                                  <div className="u-visual-bk"></div>
+                                  <div className="img-overlay"></div>
+                                </div>
+                                <div className="abs-video-btn"></div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="scroll-dropdown-bottom-content">
+                            <div className="u-text-style-main">
+                              &quot;Shepherd exceeded all expectations as
+                              the insurance partner for our client&apos;s
+                              $315M Virginia data center project. Their
+                              speed & adaptability impressed us
+                              most—delivering a quote within 24 hours
+                              which allowed us to work efficiently with
+                              our client and demonstrated their commitment
+                              to the program.&quot;
+                              <br />
+                              <br />
+                              Sam Russell
+                              <br />
+                              Insurance Broker, WTW
+                            </div>
+                            <div>
+                              <div
+                                data-wf--button-main--variant="primary"
+                                className="button_main_wrap"
+                                data-button=""
+                                data-trigger="hover-focus"
+                              >
+                                <div className="clickable_wrap u-cover-absolute">
+                                  <a
+                                    href="/case-studies/data-center-developer-partners-opts-into-shepherd-savings-for-315m-ocip-in-va"
+                                    className="clickable_link w-inline-block"
+                                  >
+                                    <span className="clickable_text u-sr-only">
+                                      READ MORE
+                                    </span>
+                                  </a>
+                                  <button
+                                    type="button"
+                                    className="clickable_btn"
+                                  >
+                                    <span className="clickable_text u-sr-only">
+                                      READ MORE
+                                    </span>
+                                  </button>
+                                </div>
+                                <div className="button_main_element">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="100%"
+                                    viewBox="0 0 12 16"
+                                    fill="none"
+                                    className="video-play"
+                                  >
+                                    <path
+                                      d="M11.25 7.79443L2.19575e-07 15.5887L9.00968e-07 0.000204548L11.25 7.79443Z"
+                                      fill="black"
+                                    ></path>
+                                  </svg>
+                                  <div
+                                    aria-hidden="true"
+                                    className="button_main_text u-text-style-small"
+                                  >
+                                    READ MORE
+                                  </div>
+                                  <div className="button_main_icon u-hide-if-empty"></div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="scroll-dropdown-item">
+                      <div className="scroll-dropdown-top">
+                        <div className="scroll-dropdown-top-text u-text-style-h4">
+                          Primary & Lead Excess for $255M for battery
+                          manufacturer construction in Washington
+                        </div>
+                      </div>
+                      <div className="scroll-dropdown-bottom-wrap">
+                        <div className="scroll-dropdown-bottom-grid u-grid-custom">
+                          <div className="scroll-dropdown-bottom-video-col u-column-span-2">
+                            <div>
+                              <div
+                                video-parent=""
+                                className="scroll-dropdown-bottom-video-wrap"
+                              >
+                                <div
+                                  id="careers-video"
+                                  className="u-visual"
+                                >
+                                  <img
+                                    src="/assets/images/roberto-sorin-zz3qxwfznrg-unsplash.avif"
+                                    loading="lazy"
+                                    alt=""
+                                    sizes="(max-width: 1360px) 100vw, 1360px"
+                                    srcSet="/assets/images/roberto-sorin-zz3qxwfznrg-unsplash-p-500.avif 500w, /assets/images/roberto-sorin-zz3qxwfznrg-unsplash-p-800.avif 800w, /assets/images/roberto-sorin-zz3qxwfznrg-unsplash-p-1080.avif 1080w, /assets/images/roberto-sorin-zz3qxwfznrg-unsplash-p-1600.avif 1600w, /assets/images/roberto-sorin-zz3qxwfznrg-unsplash.avif 1920w"
+                                    className="u-visual-image"
+                                  />
+                                  <div className="u-visual-bk"></div>
+                                  <div className="img-overlay"></div>
+                                </div>
+                                <div className="abs-video-btn"></div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="scroll-dropdown-bottom-content">
+                            <div className="u-text-style-main">
+                              Shepherd launched capabilities to support
+                              Primary Casualty coverage for contractor
+                              practice programs and projects, including
+                              wrap-ups and JVs. These new products
+                              complement Shepherd&apos;s existing Excess
+                              program.
+                            </div>
+                            <div>
+                              <div
+                                data-wf--button-main--variant="primary"
+                                className="button_main_wrap"
+                                data-button=""
+                                data-trigger="hover-focus"
+                              >
+                                <div className="clickable_wrap u-cover-absolute">
+                                  <a
+                                    href="/case-studies/primary-lead-excess-for-255m-for-battery-manufacturer-construction-in-washington"
+                                    className="clickable_link w-inline-block"
+                                  >
+                                    <span className="clickable_text u-sr-only">
+                                      READ MORE
+                                    </span>
+                                  </a>
+                                  <button
+                                    type="button"
+                                    className="clickable_btn"
+                                  >
+                                    <span className="clickable_text u-sr-only">
+                                      READ MORE
+                                    </span>
+                                  </button>
+                                </div>
+                                <div className="button_main_element">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="100%"
+                                    viewBox="0 0 12 16"
+                                    fill="none"
+                                    className="video-play"
+                                  >
+                                    <path
+                                      d="M11.25 7.79443L2.19575e-07 15.5887L9.00968e-07 0.000204548L11.25 7.79443Z"
+                                      fill="black"
+                                    ></path>
+                                  </svg>
+                                  <div
+                                    aria-hidden="true"
+                                    className="button_main_text u-text-style-small"
+                                  >
+                                    READ MORE
+                                  </div>
+                                  <div className="button_main_icon u-hide-if-empty"></div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="forced-scroll-section-triggers">
+              <div className="trigger"></div>
+              <div className="trigger"></div>
+              <div className="trigger"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
