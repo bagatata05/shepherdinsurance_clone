87c15a21fa9fd96280cad01795a5090c2c12c248
Naphier Awalie <naphiera@gmail.com>
2026-06-23 18:16:47 +0800

    Create RenewableHero.tsx

---
+++ src/app/industry/renewable-energy-power/components/RenewableHero.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import React from "react";
+
+export const RenewableHero: React.FC = () => {
+  return (
+    <section className="u-section u-theme-brand">
+      <div className="pages-hero-grid u-grid-custom">
+        <div className="hero-grid-left">
+          <div className="hero-grid-left-max">
+            <div className="section_spacer is-pages-hero">
+              <div className="u-container is-hero">
+                <div className="u-content v-flex-8">
+                  <div className="content-flex v-flex-8 u-justify-content-between">
+                    <div className="pages-heading-flex v-flex-6">
+                      <h1 className="pages-hero-heading u-text-style-h2">
+                        Protection built{" "}
+                        <span className="txt-icon-arrow is-up">&nbsp;</span>{" "}
+                        for renewable energy and power.
+                      </h1>
+                      <div className="hero-p u-text-style-main">
+                        Protecting the infrastructure powering the next
+                        generation of the grid.
+                      </div>
+                    </div>
+                    <div className="u-button-wrapper u-margin-top-0">
+                      <div className="u-display-contents">
+                        <div
+                          data-wf--button-main--variant="primary"
+                          className="button_main_wrap"
+                          data-button=""
+                          data-trigger="hover-focus"
+                        >
+                          <div className="clickable_wrap u-cover-absolute">
+                            <a
+                              href="/contact"
+                              className="clickable_link w-inline-block"
+                            >
+                              <span className="clickable_text u-sr-only">
+                                Contact us
+                              </span>
+                            </a>
+                            <button
+                              type="button"
+                              className="clickable_btn"
+                            >
+                              <span className="clickable_text u-sr-only">
+                                Contact us
+                              </span>
+                            </button>
+                          </div>
+                          <div className="button_main_element">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="100%"
+                              viewBox="0 0 12 16"
+                              fill="none"
+                              className="video-play"
+                            >
+                              <path
+                                d="M11.25 7.79443L2.19575e-07 15.5887L9.00968e-07 0.000204548L11.25 7.79443Z"
+                                fill="black"
+                              ></path>
+                            </svg>
+                            <div
+                              aria-hidden="true"
+                              className="button_main_text u-text-style-small"
+                            >
+                              Contact us
+                            </div>
+                            <div className="button_main_icon u-hide-if-empty"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hero-grid-visual">
+          <div id="careers-video" className="u-visual">
+            <img
+              src="/assets/images/american-public-power-association-xgazzylzn18-unsplash.avif"
+              loading="lazy"
+              alt=""
+              sizes="(max-width: 1360px) 100vw, 1360px"
+              srcSet="/assets/images/american-public-power-association-xgazzylzn18-unsplash-p-500.avif 500w, /assets/images/american-public-power-association-xgazzylzn18-unsplash-p-800.avif 800w, /assets/images/american-public-power-association-xgazzylzn18-unsplash-p-1080.avif 1080w, /assets/images/american-public-power-association-xgazzylzn18-unsplash-p-1600.avif 1600w, /assets/images/american-public-power-association-xgazzylzn18-unsplash.avif 1920w"
+              className="u-visual-image"
+            />
+            <div className="u-visual-bk"></div>
+            <div className="img-overlay"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
