d4e7cb9109ffc348996982b30522cfac9ef5cd3e
Naphier Awalie <naphiera@gmail.com>
2026-06-23 18:16:45 +0800

    Create SavingsPromo.tsx

---
+++ src/app/industry/renewable-energy-power/components/SavingsPromo.tsx
@@ -0,0 +1,242 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+export const SavingsPromo: React.FC = () => {
+  return (
+    <section className="u-section u-theme-light">
+      <div className="section_spacer">
+        <div className="u-container">
+          <div className="u-content v-flex-8">
+            <div className="product-large-img-wrap">
+              <div pt-child="" className="product-large-img-content">
+                <h1 className="section-heading u-text-style-h3">
+                  Shepherd Savings Eligible Partnerships
+                </h1>
+                <div className="product-large-img-grid u-grid-custom">
+                  <div className="div-block-19">
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">PROCORE</div>
+                    </div>
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">AUTODESK</div>
+                    </div>
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">RAKEN</div>
+                    </div>
+                  </div>
+                  <div className="div-block-19">
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">OpenSpace</div>
+                    </div>
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">DroneDeploy</div>
+                    </div>
+                  </div>
+                  <div className="div-block-19">
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">Samsara</div>
+                    </div>
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">Motive</div>
+                    </div>
+                  </div>
+                  <div className="div-block-19">
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">Brickeye</div>
+                    </div>
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">WINT</div>
+                    </div>
+                    <div className="industry-eligble-flex">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 17 34"
+                        fill="none"
+                        className="small-txt-icon"
+                      >
+                        <path
+                          d="M16.9706 16.9705L2.07015e-05 -0.00010423L1.85557e-05 16.9705L1.641e-05 33.941L16.9706 16.9705Z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                      <div className="u-text-style-large">Kairos</div>
+                    </div>
+                  </div>
+                </div>
+                <div id="w-node-_46f25055-fbef-43db-4bd8-a0229d88c9a6-9d88c978">
+                  <div
+                    data-wf--button-main--variant="primary"
+                    className="button_main_wrap"
+                    data-button=""
+                    data-trigger="hover-focus"
+                  >
+                    <div className="clickable_wrap u-cover-absolute">
+                      <a
+                        href="/savings"
+                        className="clickable_link w-inline-block"
+                      >
+                        <span className="clickable_text u-sr-only">
+                          Explore Shepherd Savings
+                        </span>
+                      </a>
+                      <button type="button" className="clickable_btn">
+                        <span className="clickable_text u-sr-only">
+                          Explore Shepherd Savings
+                        </span>
+                      </button>
+                    </div>
+                    <div className="button_main_element">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="100%"
+                        viewBox="0 0 12 16"
+                        fill="none"
+                        className="video-play"
+                      >
+                        <path
+                          d="M11.25 7.79443L2.19575e-07 15.5887L9.00968e-07 0.000204548L11.25 7.79443Z"
+                          fill="black"
+                        ></path>
+                      </svg>
+                      <div
+                        aria-hidden="true"
+                        className="button_main_text u-text-style-small"
+                      >
+                        Explore Shepherd Savings
+                      </div>
+                      <div className="button_main_icon u-hide-if-empty"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="careers-video" className="u-visual">
+                <img
+                  src="/assets/images/getty-images-vexwfefzrne-unsplash-1.webp"
+                  loading="lazy"
+                  alt=""
+                  sizes="(max-width: 1360px) 100vw, 1360px"
+                  srcSet="/assets/images/getty-images-vexwfefzrne-unsplash-1-p-500.webp 500w, /assets/images/getty-images-vexwfefzrne-unsplash-1-p-800.webp 800w, /assets/images/getty-images-vexwfefzrne-unsplash-1-p-1080.webp 1080w, /assets/images/getty-images-vexwfefzrne-unsplash-1-p-1600.webp 1600w, /assets/images/getty-images-vexwfefzrne-unsplash-1-p-2000.webp 2000w, /assets/images/getty-images-vexwfefzrne-unsplash-1-p-2600.webp 2600w, /assets/images/getty-images-vexwfefzrne-unsplash-1-p-3200.webp 3200w, /assets/images/getty-images-vexwfefzrne-unsplash-1.webp 4096w"
+                  className="u-visual-image"
+                />
+                <div className="u-visual-bk"></div>
+                <div className="img-overlay"></div>
+              </div>
+              <div className="div-block-22"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
